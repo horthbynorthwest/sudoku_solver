977b86b4aabca11031254c4d890919dfadffd347
Georgie Horth <georgiehorth@gmail.com>
2021-02-18 11:21:04 +0000

    Added method that returns the location of a 0 value space

--- solver.py
+++ solver.py
@@ -10,18 +10,22 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
-def print_board(bo):
-    for i in range(len(bo)):
+def print_board(board):
+    for i in range(len(board)):
         if i % 3 == 0 and i != 0:
             print("- - - - - - - - - - - - - ")
 
-        for j in range(len(bo[0])):
+        for j in range(len(board[0])):
             if j % 3 == 0 and j != 0:
                 print(" | ", end="")
 
             if j == 8:
-                print(bo[i][j])
+                print(board[i][j])
             else:
-                print(str(bo[i][j]) + " ", end="")
+                print(str(board[i][j]) + " ", end="")
 
-print_board(board)
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j) # row, col - normally col, row so here for memory
